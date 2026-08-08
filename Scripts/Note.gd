f32351d6195ad5eca7ff490eeb8ff3fe57f05dee
Mangod4435 <nathanonk29@gmail.com>
2026-08-08 20:23:06 +0700

    Add BIG-SHOT audio; speed/spawn and chart fixes
    Add BIG-SHOT_DELTARUNE.mp3 and its .import, update ChartTest.json to reference the music and a delay, and add an AudioStreamPlayer to MainGame.tscn. Increase note SPEED to 10 and correct the Note scene's script placement. Move chart loading out of _ready in NotesSpawner and fix spawn X calculation (use 1000). Remove the obsolete import file and rename the music asset.

--- Scripts/Note.gd
+++ Scripts/Note.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-const SPEED = 7.5
+const SPEED = 10
 
 func _physics_process(_delta: float) -> void:
 	position.x -= SPEED 
--- Scripts/NotesSpawner.gd
+++ Scripts/NotesSpawner.gd
@@ -1,14 +1,15 @@
 extends Node2D
 
+var this_chart = JSONReader.LoadJson("res://Charts/ChartTest.json")
+
 func _ready() -> void:
-	var this_chart = JSONReader.LoadJson("res://Charts/ChartTest.json")
 	var posistions = this_chart.notes
 	for note in posistions:
 		var templ: Node2D
 		match int(note.t):
 			0:
 				templ = preload("res://Scenes/Note.tscn").instantiate() as Node2D
-		var output_pos = Vector2(note["x"] + position.x, 0)
+		var output_pos = Vector2(note["x"] + 1000, 0)
 		match int(note["y"]):
 			0:
 				output_pos.y = 360
