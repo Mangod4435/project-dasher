1eb41b2ce3f2d2c186a4064960e292693900e103
Mangod4435 <nathanonk29@gmail.com>
2026-09-03 20:28:33 +0700

    feat: Add type checking and some unfinished(i guess) judgement

--- Scripts/BaseLine.gd
+++ Scripts/BaseLine.gd
@@ -16,9 +16,17 @@ func _on_area_exited(area: Area2D):
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey and event.is_pressed() and overlapping.size() != 0:
-		if !event.keycode == KEY_ESCAPE && !event.is_echo():
+		if !event.keycode == KEY_ESCAPE and !event.is_echo() and overlapping[0].get_meta("Type") == "Tap": # tap note and head
 			var offset = abs(overlapping[0].global_position.x - position.x)
 			var score = abs(50 - offset)
 			overlapping[0].queue_free()
 			overlapping.remove_at(0)
 			ScoreManager.currentScore += score
+		elif event.is_released() and overlapping[0].get_meta("Type") == "Tail":
+			var offset = overlapping[0].global_position.x - position.x * 2
+			if offset > -200 * overlapping[0].scale.x:
+				overlapping[0].queue_free()
+				ScoreManager.currentScore -= 50
+		elif (event.is_pressed() and event.is_echo()) or event.is_released() and overlapping[0].get_meta("Type") == "End":
+			var _offset = overlapping[0].global_position.x - position.x
+			
