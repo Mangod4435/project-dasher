6b7636e7833bed3feb1e002641c0551eb79963ba
Mangod4435 <nathanonk29@gmail.com>
2026-09-07 20:49:37 +0700

    feat: add signal for note touching baseline

--- Scripts/Note.gd
+++ Scripts/Note.gd
@@ -1,8 +1,17 @@
-extends Node2D
+extends Area2D
 
 const SPEED = 10
+signal on_note_touch_baseline
 
+func _ready() -> void:
+	area_entered.connect(_on_area_enter)
+	pass
 func _physics_process(_delta: float) -> void:
 	if global_position.x < 0:
 		queue_free()
 	position.x -= SPEED 
+
+func _on_area_enter(area: Area2D):
+	if area.get_script().resource_name == "BaseLine.gd":
+		on_note_touch_baseline.emit()
+		pass
