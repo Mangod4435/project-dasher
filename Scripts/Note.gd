df0ae7f58ede63e32d1ed1a7938d91deb19eb9ac
Mangod4435 <nathanonk29@gmail.com>
2026-09-09 12:52:08 +0700

    feat: connect _on_note_touch to on_note_touch_baseline

--- Scripts/Note.gd
+++ Scripts/Note.gd
@@ -4,14 +4,17 @@ const SPEED = 10
 signal on_note_touch_baseline
 
 func _ready() -> void:
+	on_note_touch_baseline.connect(_on_note_touch)
 	area_entered.connect(_on_area_enter)
-	pass
+
 func _physics_process(_delta: float) -> void:
 	if global_position.x < 0:
 		queue_free()
 	position.x -= SPEED 
 
+func _on_note_touch():
+	print("Note touched baseline!!!!!!!!!!!!")
+
 func _on_area_enter(area: Area2D):
 	if area.get_script().resource_name == "BaseLine.gd":
 		on_note_touch_baseline.emit()
-		pass
