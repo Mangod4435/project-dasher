bbc3cb8e68f1e68b274f04dd6f87606b9f09759a
Mangod4435 <nathanonk29@gmail.com>
2026-09-10 19:19:09 +0700

    feat: add implement for the _on_note_touch
    IDK why but pipis keep changing their filename... maybe it's a spamton's curse(someone pls explain)

--- Scripts/Note.gd
+++ Scripts/Note.gd
@@ -3,6 +3,7 @@ extends Area2D
 const SPEED = 10
 signal on_note_touch_baseline
 var inp_event: InputEvent
+var BaseLine_x_pos: float
 
 func _unhandled_input(event: InputEvent) -> void:
 	inp_event = event
@@ -10,6 +11,7 @@ func _unhandled_input(event: InputEvent) -> void:
 func _ready() -> void:
 	on_note_touch_baseline.connect(_on_note_touch)
 	area_entered.connect(_on_area_enter)
+	BaseLine_x_pos = get_node("/root/GameScene/BaseLine").position.x
 
 func _physics_process(_delta: float) -> void:
 	if global_position.x < 0:
@@ -17,7 +19,19 @@ func _physics_process(_delta: float) -> void:
 	position.x -= SPEED
 
 func _on_note_touch():
-	print("Note touched baseline!!!!!!!!!!!!")
+		if inp_event is InputEventKey and inp_event.is_pressed():
+			if !inp_event.keycode == KEY_ESCAPE and !inp_event.is_echo() and self.get_meta("Type") == "Tap": # tap note and head
+				var offset = abs(self.global_position.x - BaseLine_x_pos)
+				var score = abs(100 - offset)
+				queue_free()
+				ScoreManager.currentScore += score
+			elif inp_event.is_released() and self.get_meta("Type") == "Tail":
+				var offset = global_position.x - BaseLine_x_pos * 2
+				if offset > -200 * scale.x:
+					queue_free()
+					ScoreManager.currentScore -= 100
+			elif inp_event.is_pressed() or inp_event.is_released() and get_meta("Type") == "End":
+				ScoreManager.currentScore += 100
 
 func _on_area_enter(area: Area2D):
 	if area.is_in_group("BaseLine"):
