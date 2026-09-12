4e043d9f41b8f6963cf8e6652ad6523a2e857b78
Mangod4435 <nathanonk29@gmail.com>
2026-09-12 20:56:12 +0700

    refactor: Even more simplify in note.gd

--- Scripts/Note.gd
+++ Scripts/Note.gd
@@ -2,16 +2,16 @@ extends Area2D
 
 const SPEED = 10
 signal on_note_touch_baseline
-var inp_event: InputEvent
-var BaseLine_x_pos: float
+var _inp_event: InputEvent
+var _BaseLine_x_pos: float
 
 func _unhandled_input(event: InputEvent) -> void:
-	inp_event = event
+	_inp_event = event
 
 func _ready() -> void:
 	on_note_touch_baseline.connect(_on_note_touch)
 	area_entered.connect(_on_area_enter)
-	BaseLine_x_pos = get_node("/root/GameScene/BaseLine").position.x
+	_BaseLine_x_pos = get_node("/root/GameScene/BaseLine").position.x
 
 func _physics_process(_delta: float) -> void:
 	if global_position.x < 0:
@@ -19,16 +19,19 @@ func _physics_process(_delta: float) -> void:
 	position.x -= SPEED
 
 func _on_note_touch():
-	if inp_event is InputEventKey:
-		var _pressed: bool = inp_event.is_pressed() and inp_event.keycode != KEY_ESCAPE
-		if !inp_event.is_echo() and get_meta("Type") == "Tap" and _pressed:
-			var offset = abs(global_position.x - BaseLine_x_pos)
+	if _inp_event is InputEventKey:
+		var _pressed: bool = _inp_event.is_pressed() and _inp_event.keycode != KEY_ESCAPE
+		var _released: bool = _inp_event.is_released()
+		var _meta: String = get_meta("Type")
+		if !_inp_event.is_echo() and _meta == "Tap" and _pressed:
+			var offset = abs(global_position.x - _BaseLine_x_pos)
 			var score = abs(100 - offset)
 			ScoreManager.currentScore += score
 			queue_free()
-		elif (_pressed or inp_event.is_released()) and get_meta("Type") == "End":
+		elif (_pressed or _released) and _meta == "End":
 			ScoreManager.currentScore += 100
-		elif inp_event.is_released() and get_meta("Type") == "Tail":
+			queue_free()
+		elif _released and _meta == "Tail":
 			ScoreManager.currentScore -= 100
 			queue_free()
 
