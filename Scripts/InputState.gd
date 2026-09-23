65c1fb2d4263d85e11b4c90041ec1de09ea066bc
Mangod4435 <nathanonk29@gmail.com>
2026-09-24 00:15:44 +0700

    refactor: remove the get_curr_keycode useless function

--- Scripts/InputState.gd
+++ Scripts/InputState.gd
@@ -1,19 +1,11 @@
 extends Node
 
 var held_keys: int = 0
-var current_keycode: Key
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey and not event.echo:
-		if event.pressed:
-			held_keys += 1
-			current_keycode = event.keycode
-		elif event.is_released():
-			held_keys = max(0, held_keys - 1)
-			current_keycode = KEY_NONE
+		if event.pressed: held_keys += 1
+		elif event.is_released(): held_keys = max(0, held_keys - 1)
 
 func is_any_key_held() -> bool:
 	return held_keys > 0
-
-func get_curr_keycode() -> Key:
-	return current_keycode
